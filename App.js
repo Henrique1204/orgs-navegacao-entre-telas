80db15fbd5687ba5ccfbf485b9f381d8192f946b
Paulo <pauloh16.pdr@gmail.com>
2022-05-19 21:35:32 -0300

    Separar as rotas em um arquivo unico

--- App.js
+++ App.js
@@ -1,32 +1,18 @@
 import React from 'react';
 import {StatusBar, SafeAreaView} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import Home from './src/telas/Home';
-import MelhoresProdutores from './src/telas/MelhoresProdutores';
+import AppRotas from './src/appRotas';
 
 const style = {
   flex: 1,
 };
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <SafeAreaView style={style}>
       <StatusBar />
 
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home} />
-
-          <Tab.Screen
-            name="Melhores Produtores"
-            component={MelhoresProdutores}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
+        <AppRotas />
     </SafeAreaView>
   );
 }
