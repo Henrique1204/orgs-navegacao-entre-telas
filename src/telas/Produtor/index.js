15e30e7b110295d9d9b89a4bda8a4cfa914b275f
Paulo <pauloh16.pdr@gmail.com>
2022-05-20 21:22:06 -0300

    Compartilhar informacoes entre rotas

--- src/telas/Produtor/index.js
+++ src/telas/Produtor/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactNative from 'react-native';
 
+import {useRoute} from '@react-navigation/native';
+
 const Produtor = () => {
+  const {params} = useRoute();
+
+  console.log(params);
+
   return <ReactNative.Text>Opa</ReactNative.Text>;
 };
 
