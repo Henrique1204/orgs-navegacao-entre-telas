2c5792e0244b6f9c0c9832ff395dd23b18e0afac
Paulo <pauloh16.pdr@gmail.com>
2022-05-20 22:24:30 -0300

    Ajustar icones da tab bottom

--- src/telas/Rotas/appRotas.js
+++ src/telas/Rotas/appRotas.js
@@ -6,12 +6,29 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import ProdutorRotas from './ProdutorRotas';
 import MelhoresProdutoresRotas from './MelhoresProdutoresRotas';
 
+import Coracao from '../../assets/coracao.svg';
+import Home from '../../assets/home.svg';
+
 const Tab = createBottomTabNavigator();
+
+const IconsTab = {
+  'Melhores Produtores': Coracao,
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Navigator
+        screenOptions={({route: {name}}) => ({
+          headerShown: false,
+          tabBarActiveTintColor: '#2A9F85',
+          tabBarInactiveTintColor: '#C7C7C7',
+          tabBarIcon: ({color}) => {
+            const Icon = IconsTab?.[name] || Home;
+
+            return <Icon color={color} />;
+          },
+        })}>
         <Tab.Screen name="Home" component={ProdutorRotas} />
 
         <Tab.Screen
